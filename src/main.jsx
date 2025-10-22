764c4545e1bc1a924cc5e40a901e241c6afa9db4
Parnian khajevand <parnyan.khajevand@gmail.com>
2025-10-22 20:32:46 +0330

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+/*import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)*/
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import ProductPage from "./ProductPage.jsx";  // این فایل رو باید بسازی
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
+
+
