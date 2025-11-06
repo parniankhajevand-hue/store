fe4f88f987cbee4e3b4974b17c829aef991da20d
Parnian khajevand <parnyan.khajevand@gmail.com>
2025-11-06 13:22:46 +0330

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,14 @@
-/*import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)*/
-
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import ProductPage from "./ProductPage.jsx";  // این فایل رو باید بسازی
+import "./index.css"; 
+import "./app.css"; 
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/product/:id" element={<ProductPage />} />
-      </Routes>
+      <App />
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
-
-
